85067497824e8262426a185123c0484d7a8902de
joshu <joshuapetitma@yahoo.com>
2020-10-14 06:56:54 -0400

    fix: regex

--- biblehub/scrape_utils.py
+++ biblehub/scrape_utils.py
@@ -3,12 +3,15 @@ import re
 
 def parse_str(reference: str) -> dict:
     response = {}
-    parsed = re.search('(\\d? ?\\w+) (\\d+)(:(\\d+)-(\\d+))?', reference)
+    parsed = re.search('([1-3]?[^\\s]+[A-Za-z ]+[^\\d ]) ?(\\d+)?:?(\\d+)?-?(\\d+)?', reference)
     response['book'] = parsed.group(1).lower()
     response['chapter'] = int(parsed.group(2))
     response['start_verse'] = 1
     response['end_verse'] = None
-    if len(parsed.groups()) == 5 and parsed.group(4) is not None and parsed.group(5) is not None:
-        response['start_verse'] = int(parsed.group(4))
-        response['end_verse'] = int(parsed.group(5))
+    print(parsed.groups())
+    if parsed.group(3) is not None:
+        response['start_verse'] = int(parsed.group(3))
+    if parsed.group(4) is not None:
+        response['end_verse'] = int(parsed.group(4))
     return response
+
